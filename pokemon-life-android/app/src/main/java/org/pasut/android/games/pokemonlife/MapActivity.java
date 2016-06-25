abbd6404330809b05f4a02dd38dfa85b3a1542e6
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-06-25 14:22:16 -0300

    Creo la aplicacion android y le agrego el mapa

---
+++ pokemon-life-android/app/src/main/java/org/pasut/android/games/pokemonlife/MapActivity.java
@@ -0,0 +1,26 @@
+package org.pasut.android.games.pokemonlife;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
